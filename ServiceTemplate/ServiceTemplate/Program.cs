b09029e25eedac2bc5ca1770e47cc8d160e6cbd0
lausai <lausai360@gmail.com>
2017-07-29 15:45:24 +0800

    Add project

---
+++ ServiceTemplate/ServiceTemplate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ServiceTemplate
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            if (!Environment.UserInteractive)
+            {
+                Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] 
+                { 
+				    new Service() 
+                };
+
+                ServiceBase.Run(ServicesToRun);
+            }
+            else
+            {
+                var service = new Service();
+                service.ToStart();
+
+                WaitForExit();
+
+                service.ToStop();
+            }
+        }
+
+        static void WaitForExit()
+        {
+            while (true)
+            {
+                if (Console.ReadLine().ToUpper() == "QUIT")
+                    break;
+            }
+        }
+    }
+}
---
+++ ServiceTemplate/ServiceTemplate/Service.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ServiceTemplate
+{
+    public partial class Service : ServiceBase
+    {
+        public Service()
+        {
+            InitializeComponent();
+        }
+
+        public void ToStart()
+        {
+        }
+
+        public void ToStop()
+        {
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ToStart();
+        }
+
+        protected override void OnStop()
+        {
+            ToStop();
+        }
+    }
+}
